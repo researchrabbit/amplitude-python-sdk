0872dccacd062ea83daf7206a082b038e2105356
Vaggelis-Sotiropoulos <vsotiropoulo@gmail.com>
2021-04-27 18:31:58 -0700

    chartsapi - Adds ChartsAnnotationsV2 model and client

---
+++ amplitude_python_sdk/v2/models/charts.py
@@ -0,0 +1,18 @@
+"""
+This module contains all models relating to the Amplitude V2 Chart Annotations API.
+
+See <https://developers.amplitude.com/docs/chart-annotations-api> for documentation.
+"""
+
+from datetime import date
+from typing import Optional
+
+from pydantic import BaseModel  # pylint: disable=no-name-in-module
+
+
+class ChartAnnotationsV2(BaseModel):  # pylint: disable=too-few-public-methods
+    app_id: int
+    date: date  # YYYY-MM-DD
+    label: str
+    chart_id: Optional[str] = None
+    details: Optional[str] = None
