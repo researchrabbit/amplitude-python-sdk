04379724ac242a0a1f32e3245481f02cea966395
Krishnan Chandra <krishnan.chandra@gmail.com>
2021-04-30 08:03:00 -0400

    Add some tests for the util functions

---
+++ amplitude_python_sdk/common/utils/test_utils.py
@@ -0,0 +1,28 @@
+"""Test file for the functions in the utils package."""
+
+import pytest
+from requests import Response
+
+from . import return_or_raise
+from ..exceptions import AmplitudeAPIException
+
+
+def test_return_or_raise_failed_status():
+    """Test a response with a failure status, and ensure that it correctly fails."""
+    response = Response()
+    response.status_code = 400
+    response._content = (  # pylint: disable=protected-access
+        b'{"error": "Failed to call Amplitude"}'
+    )
+    with pytest.raises(AmplitudeAPIException):
+        return_or_raise(response)
+
+
+def test_return_or_raise_success():
+    """Test a response with a success status, and ensure that it does not fail."""
+    response = Response()
+    response.status_code = 200
+    response._content = (  # pylint: disable=protected-access
+        b'{"error": "Failed to call Amplitude"}'
+    )
+    assert return_or_raise(response) == response
