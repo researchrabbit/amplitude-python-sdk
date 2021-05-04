a9961127a9d4cc0e402ecadad376d49b7da54eaa
Krishnan Chandra <krishnan.chandra@gmail.com>
2021-05-04 00:06:02 -0400

    Remove custom payload generation functions

--- amplitude_python_sdk/v1/models/identify.py
+++ amplitude_python_sdk/v1/models/identify.py
@@ -2,7 +2,7 @@
 
 from typing import Optional, Dict, Any
 
-from pydantic import BaseModel  # pylint: disable=no-name-in-module
+from pydantic import BaseModel, Field  # pylint: disable=no-name-in-module
 
 from ...common.models import DeviceInfo, LocationInfo, EventIdentifier
 
@@ -41,45 +41,20 @@ class UserProperties(BaseModel):  # pylint: disable=too-few-public-methods
     corresponding properties that you want to operate on."
     """
 
-    set_fields: Optional[Dict[str, Any]] = None
-    set_once_fields: Optional[Dict[str, Any]] = None
-    add_fields: Optional[Dict[str, Any]] = None
-    append_fields: Optional[Dict[str, Any]] = None
-    prepend_fields: Optional[Dict[str, Any]] = None
-    unset_fields: Optional[Dict[str, Any]] = None
-    pre_insert_fields: Optional[Dict[str, Any]] = None
-    post_insert_fields: Optional[Dict[str, Any]] = None
-    remove_fields: Optional[Dict[str, Any]] = None
-
-    @property
-    def payload(self):
-        """
-        Converts the class fields into a request payload
-        that the Amplitude API will accept.
-        """
-        output = {}
-        if self.set_fields:
-            output["$set"] = self.set_fields
-        if self.set_once_fields:
-            output["$setOnce"] = self.set_once_fields
-        if self.add_fields:
-            output["$add"] = self.add_fields
-        if self.append_fields:
-            output["$append"] = self.append_fields
-        if self.prepend_fields:
-            output["$prepend"] = self.prepend_fields
-        if self.unset_fields:
-            output["$unset"] = self.unset_fields
-        if self.pre_insert_fields:
-            output["$preInsert"] = self.unset_fields
-        if self.post_insert_fields:
-            output["$postInsert"] = self.unset_fields
-        if self.remove_fields:
-            output["$remove"] = self.unset_fields
-        return output
+    set_fields: Optional[Dict[str, Any]] = Field(alias="$set")
+    set_once_fields: Optional[Dict[str, Any]] = Field(alias="$setOnce")
+    add_fields: Optional[Dict[str, Any]] = Field(alias="$add")
+    append_fields: Optional[Dict[str, Any]] = Field(alias="$append")
+    prepend_fields: Optional[Dict[str, Any]] = Field(alias="$prepend")
+    unset_fields: Optional[Dict[str, Any]] = Field(alias="$unset")
+    pre_insert_fields: Optional[Dict[str, Any]] = Field(alias="$preInsert")
+    post_insert_fields: Optional[Dict[str, Any]] = Field(alias="$postInsert")
+    remove_fields: Optional[Dict[str, Any]] = Field(alias="$remove")
 
 
-class Identification(DeviceInfo, EventIdentifier, LocationInfo):
+class Identification(
+    DeviceInfo, EventIdentifier, LocationInfo
+):  # pylint: disable=too-few-public-methods
     """
     See <https://developers.amplitude.com/docs/identify-api#keys-for-the-identification-argument>
     for documentation.
@@ -89,14 +64,3 @@ class Identification(DeviceInfo, EventIdentifier, LocationInfo):
     paying: Optional[str] = None
     start_version: Optional[str] = None
     user_properties: Optional[UserProperties] = None
-
-    @property
-    def payload(self) -> dict:
-        """
-        Generates the payload (as a Python dictionary) required by the
-        Amplitude API for identifying a user.
-        """
-        base_dict = self.dict(exclude={"user_properties"}, exclude_none=True)
-        if self.user_properties:
-            base_dict["user_properties"] = self.user_properties.payload
-        return base_dict
